26aca09b082b37be1de21624dc425ea21cd9e96d
mahdi7912 <salahshormahdi@gmail.com>
2024-04-02 03:22:46 +0330

    beatiful

--- bot.py
+++ bot.py
@@ -2,15 +2,15 @@ from pyrogram import Client , filters
 import imdb
 from pyrogram.types import Message, ReplyKeyboardMarkup , ReplyKeyboardRemove  ,InputTextMessageContent,InlineKeyboardMarkup , InlineKeyboardButton , InlineQuery , CallbackQuery, InlineQueryResultArticle, InlineQueryResult
 
-# Proxy = { "scheme": "socks5",
-# "hostname": "127.0.0.1",
-# "port": 10808}
+Proxy = { "scheme": "socks5",
+"hostname": "0.0.0.0",
+"port": 10000}
 
 plugins = dict(root = 'plugins')
 
 app = Client( "test_bot",
-          #    proxy=Proxy,
-          #    plugins= plugins,
+             proxy=Proxy,
+             plugins= plugins,
              api_id = 29115149 ,
              api_hash = "8fed6671ec1ccb880d22d045761f9e25" ,
              bot_token = "6932883798:AAGpel6pNCE8Kug38MIV0o7v4kTCmYlBSwc")
